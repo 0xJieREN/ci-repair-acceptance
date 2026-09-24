220d68b6bc883bd7c2d759130efa76f4df5340f6
Jared <ethan.jie.ren@gmail.com>
2026-09-24 16:44:08 +0800

    Simplify range and slug helpers

--- calc/text.py
+++ calc/text.py
@@ -3,5 +3,5 @@ import re
 
 def slugify(title: str) -> str:
     """Lowercase words joined by single hyphens; other characters are dropped."""
-    words = re.findall(r"[a-z0-9]+", title.lower())
+    words = re.findall(r"[a-z0-9]+", title)
     return "-".join(words)
